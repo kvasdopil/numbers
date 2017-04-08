4df8324ce4fa56cb0fe13ed43b7cb5d1cb792483
Alexey Guskov <lexag@mail.ru>
2017-04-08 22:42:37 +0300

    webpack works
    Signed-off-by: Alexey Guskov <lexag@mail.ru>

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// TODO: hmre
+// TODO: flow? ts?
+// TODO: server diagnostics output
+// TODO: client tests
+// TODO: server tests
+
 const App = props => 
   <div>Hello world</div>
 
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	context: __dirname + "/client",
+	entry: './index',
+	output: {
+		path: __dirname + '/static',
+		filename: 'bundle.js',
+	},
+	module: {
+		loaders: [{
+			test: /.js?$/,
+			loader: 'babel-loader',
+		}]
+	}
+  // configuration
+};
