d1a92136b6a41a614ab00564b1ad33f8368e2ecf
Alexey Guskov <lexag@mail.ru>
2017-04-09 23:49:36 +0300

    info about decorators support in babel6
    Signed-off-by: Alexey Guskov <lexag@mail.ru>

--- client/NumbersStore.js
+++ client/NumbersStore.js
@@ -1,5 +1,9 @@
 import { observable } from 'mobx';
 
+// FIXME: decorators only work with legacy plugin until babel 7
+// this breaks 'env' preset, and only works with 'es2015' + 'stage-0' presets
+// i.e. docorators work only when transpiling to es5
+
 export default class NumbersStore {
   @observable rounds = false;
 
