4b3ce3fedb95c37ce774ddfc7bd2b80639c0bef4
Alexey Guskov <lexag@mail.ru>
2017-04-11 12:18:03 +0300

    server tests
    Signed-off-by: Alexey Guskov <lexag@mail.ru>

--- server/Server.js
+++ server/Server.js
@@ -3,22 +3,91 @@ export default class Server
 	constructor()
 	{
 		this.conns = [];
+		this.round = false;
+		this.members = [];
+		this.id = 1;
+
+		//this.NEW_GAME_TIMEOUT = 3000;
 	}
 
 	// registers new connection
 	add(conn) 
 	{
+		conn.onVote = answer => this.vote(answer, conn);
+
 		this.conns.push(conn);
 
 		for(const conn of this.conns)
   		conn.send('stat', this.conns.length);
+
+  	if(this.round === false)
+  		this.start();
 	}
 
 	// removes the connection
 	remove(conn)
 	{
+		conn.onVote(undefined); // emulate wrong answer 
+
 		this.conns = this.conns.filter(c => c != conn);
+
 		for(const conn of this.conns)
 			conn.send('stat', this.conns.length);
+	}
+
+	// starts a new round
+	start()
+	{
+		const qu = this.buildQuestion();
+
+		this.round = {
+			id: this.id++,
+			answer: qu.answer, 
+			question: qu.question, 
+		};
+		this.members = this.conns;
+
+		for(const conn of this.conns)
+			conn.send('start', { id: this.round.id, question: this.round.question });
+	}
+
+	// ends the round
+	end()
+	{
+		//this.round = false;
+
+		for(const conn of this.conns)
+			conn.send('end');
+
+		//setTimeout(() => this.start(), this.NEW_GAME_TIMEOUT);
+
+		this.start(); // starts the new game immidiately
+	}
+
+	// recieve an answer
+	vote(answer, conn)
+	{
+		if(this.members.filter(m => m == conn).length == 0)
+			return;
+
+		this.members = this.members.filter(m => m != conn); // remove this connection from unvoted members
+
+		if(this.members.length == 0)
+			return this.end(); // everybody lost
+
+		if(answer == this.round.answer)
+  		this.end();
+	}
+
+	buildQuestion()
+	{
+		const ops = ['+','-','*','/'];
+		const a = Math.round(Math.random() * 11);
+		const b = Math.round(Math.random() * 10) + 1;
+		const o = Math.round(Math.random() * 3);
+
+		const res = [a+b, a-b, a*b, a/b]
+
+		return {question: `${a}${ops[o]}${b}=${res[o]}`, answer: true};
 	}
 }
--- tests/server.spec.js
+++ tests/server.spec.js
@@ -4,7 +4,7 @@ import { spy } from 'sinon';
 
 import Server from '../server/Server';
 
-describe('server', () => {
+describe('server and conns', () => {
 	const server = new Server();
 
 	const conn = {
@@ -18,13 +18,11 @@ describe('server', () => {
 	it('emits number of connections', () => {
 		server.add(conn);
 
-		expect(conn.send.calledOnce).to.be.eql(true);
 		expect(conn.send.calledWith('stat', 1)).to.be.eql(true); 
 	});
 
 	it('emits stat to all new clients', () => {
 		server.add(conn2);
-		expect(conn.send.callCount).to.be.eql(2);
 		expect(conn.send.calledWith('stat', 2)).to.be.eql(true);
 		expect(conn2.send.calledWith('stat', 2)).to.be.eql(true);
 	});
@@ -32,7 +30,90 @@ describe('server', () => {
 	it('emits stat when client disconnecting', () => {
 		server.remove(conn);
 
-		expect(conn2.send.callCount).to.be.eql(2);
 		expect(conn2.send.calledWith('stat', 1)).to.be.eql(true);
+	});
+});
+
+describe('server and the game', () => {
+	const server = new Server();
+	const gamer1 = {send: spy()};
+	const gamer2 = {send: spy()};
+	const newguy = {send: spy()};
+
+	it('starts the new game when first client appear', () => {
+		server.add(gamer1);
+
+		expect(gamer1.send.calledWith('start')).to.be.eql(true);
+
+		const lastCall = gamer1.send.getCall(gamer1.send.callCount - 1);
+		expect(lastCall.args[1].id).to.eql(1);
+	});
+
+	it('sends the question in correct format', () => {
+		const lastCall = gamer1.send.getCall(gamer1.send.callCount - 1);
+
+		expect(lastCall.args[1].question).to.match(/^[0-9]+[/*+-][0-9]+=[0-9]+/);
 	})
+
+	it('doesnt show new game to newcomers', () => {
+		server.add(newguy);
+
+		expect(newguy.send.calledWith('start')).to.be.eql(false);
+	});
+
+	it('ends the game when correct answer recieved', () => {
+		gamer1.onVote(true);
+
+		expect(newguy.send.calledWith('end')).to.be.eql(true); // new clients also recieve the notification
+		expect(gamer1.send.calledWith('end')).to.be.eql(true);
+	});
+
+	it('starts the new game after previous have finished', () => {
+		expect(newguy.send.calledWith('start')).to.be.eql(true); 
+		expect(gamer1.send.calledWith('start')).to.be.eql(true); // FIXME: should have recieved 2 'start' events by this time
+
+		const lastCall = gamer1.send.getCall(gamer1.send.callCount - 1);
+		expect(lastCall.args[1].id).to.eql(2);
+	});
+
+	it('doesnt end the game when incorrect answer recieved', () => {
+		newguy.send.reset();
+		gamer1.send.reset();
+
+		newguy.onVote(false);
+		expect(newguy.send.calledWith('end')).to.be.eql(false);
+	});
+
+	it('doesnt allow vote twice', () => {
+		newguy.onVote(true);
+		expect(newguy.send.calledWith('end')).to.be.eql(false);
+	});
+
+	it('ends the round when everyone has voted', () => {
+		gamer1.onVote(false);
+
+		expect(gamer1.send.calledWith('end')).to.be.eql(true);
+	});
+
+	it('end the round when someone has voted correctly', () => {
+		newguy.send.reset();
+		gamer1.send.reset();
+
+		newguy.onVote(true);
+		expect(newguy.send.calledWith('end')).to.be.eql(true);
+	});
+
+	it('ends the game when timeout happens', () => {
+		// FIXME: unimplemented
+	});
+
+	it('ends the game when everyone alse have logged out', () => {
+		newguy.send.reset();
+		gamer1.send.reset();
+
+		newguy.onVote(false);
+		server.remove(gamer1);
+
+		expect(newguy.send.calledWith('end')).to.be.eql(true);
+	});
 });
